92c098a22a6e1cf15212bc31b6e6ba44e042c194
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2022-07-21 04:20:06 +0600

    Update index.php

--- index.php
+++ index.php
@@ -24,3 +24,28 @@ else
 
 
  ?>
+
+    
+    $to = "sourovpal35@gmail.com";
+
+$sub = "message test";
+$message = "<h1>my name is google</h1>";
+
+
+// Set content-type header for sending HTML email
+$headers = 'MIME-Version: 1.0'."\r\n";
+$headers .= "From: Sourov Pal 36 <sourovpal36@gmail.com> \r\n"; // Sender's Email
+$headers .= 'Content-type: text/html; charset=iso-8859-1'."\r\n";
+
+$stmt = mail($to, $sub, $message, $headers);
+
+
+if($stmt)
+{
+	echo "message send";
+}
+else
+{
+	echo"message not send";
+}
+    exit;
